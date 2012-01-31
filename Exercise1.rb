4b0a3445c81e6a124c047f103f6bbe75cde77178
Matt O'Donnell <mattodonnell417@yahoo.com>
2012-01-30 23:42:35 -0700

    Added bigArray which builds an array from 1 - 1000

--- Exercise1.rb
+++ Exercise1.rb
@@ -1,12 +1,22 @@
 class Exercise1
-    
-    def initialize 
-        @today = Time.now
-    end
-    
-    def getCurrDate
-        puts @today.to_s
-    end
-    
+
+	def initialize
+ 			@today = Time.now
+	end
+	def getCurrDate
+ 		puts @today.to_s
+		end
+		 	def getBigArray
+ 		# @bigArray = []
+ 		@bigArray = Array.new
+ 		# Fill array with the numbers 1-1000
+ 		for i in 1..1000
+ 			#puts @value of local varibale is #{i}
+ 			@bigArray.push( i )
+ 		end
+ 		puts @bigArray.join(',') #outputs a string of numbers with a comma in between
+ 		#puts @bigArray
+ 	end
+
 end
 
